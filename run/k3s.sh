2e3ddc4cbfc63e5006d4eece83638a8f9874e1a4
kkorolyov <kirillk753@gmail.com>
2021-02-25 10:26:13 -0800

    Fix indent

--- run/k3s.sh
+++ run/k3s.sh
@@ -3,8 +3,8 @@
 # local registry mirror
 tee -a /etc/rancher/k3s/registries.yaml <<EOF
 mirrors:
-	dreg.local:
-		endpoint:
-			- http://dreg.local
+  dreg.local:
+    endpoint:
+      - http://dreg.local
 EOF
 echo "127.0.0.1	dreg.local" | tee -a /etc/hosts
